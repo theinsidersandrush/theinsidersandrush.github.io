04e69fcbe54e198d6d0bbb27f7158c1c49a9f5a1
The Insider Sandrush <87752859+theinsidersandrush@users.noreply.github.com>
2024-11-24 06:26:27 +0800

    Added the blog directory and changed the Social Links page appearance.

---
+++ files/js/copyheart-statement.js
@@ -0,0 +1,46 @@
+/* Written on 5th November 2024, 2142 UTC +8. On election day of Kamala Harris (Democrat), Donald J. Trump (Republican), Jill Stein (Green Party, Independent) and other candidates. Let's see who got more casts.*/
+
+/* As of 6th November 2024, we're going back to the old bad times. F*ck. */
+/*
+const texts = 
+    [
+        "is an act of love", 
+        
+        "is like being Flandre Scarlet's husband", 
+        
+        "is like being Reimu Hakurei's husband", 
+        
+        "is like being Marisa Kirisame's beloved husband", 
+        
+        "is like being a husband to two wife. Reimu Hakurei and Marisa Kirisame",
+
+        "is making Reimu Hakurei more happier than ever",
+
+        "is Marisa Kirisame",
+
+        "is the hard way that Alice will take",
+
+        "is love that intertwines with the interest of geographics, broadcasting and the rules of the nation",
+
+        "is acceptable in the Internet age",
+
+        "is Alice Margatroid's way to not getting hit with the shovel",
+
+        "is not theft",
+
+        "makes the Internet more fresh",
+
+        "is not outlawed",
+
+        "multiplies by more",
+
+        "approved",
+        
+    ];
+    
+    const randomIndex = Math.floor(Math.random() * texts.length);
+
+    document.getElementById('copyheart-statement').innerText = texts[randomIndex];
+    */
+
+    const texts = ["is an act of love","is like being Flandre Scarlet's husband","is like being Reimu Hakurei's husband","is like being Marisa Kirisame's beloved husband","is like being a husband to two wife. Reimu Hakurei and Marisa Kirisame","is making Reimu Hakurei more happier than ever","is Marisa Kirisame","is the hard way that Alice will take","is love that intertwines with the interest of geographics, broadcasting and the rules of the nation","is acceptable in the Internet age","is Alice Margatroid's way to not getting hit with the shovel","is not theft","makes the Internet more fresh","is not outlawed","multiplies by more","approved",];const randomIndex = Math.floor(Math.random() * texts.length); document.getElementById('copyheart-statement').innerText = texts[randomIndex];
