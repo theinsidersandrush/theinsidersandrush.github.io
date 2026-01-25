fbc9e8fa91c06c6f6575925769b3cc15ac6c42c2
The Insider Sandrush <theinsidersandrush@gmail.com>
2026-01-26 04:26:03 +0800

    Due to timing that's have less than two weeks left, I added the 6th anniversary page for The Insider Sandrush.

--- files/js/copyheart-statement.js
+++ files/js/copyheart-statement.js
@@ -4,23 +4,43 @@ const texts =
         
         "is like being Flandre Scarlet's husband", 
         
-        "is like being Reimu Hakurei's husband", 
+        "is like being Reimu Hakurei's husband",
+
+        "is like being Kayoko Onikata's epic husband",
         
         "is like being Marisa Kirisame's beloved husband", 
         
+        "is like being Suzumi Morizuki's beloved husband", 
+
         "is like being a husband to two wife. Reimu Hakurei and Marisa Kirisame",
+
+        "is like being a husband to two wife. Suzumi Morizuki and Kayoko Onikata",
 
         "is making Reimu Hakurei more happier than ever",
 
-        "is Marisa Kirisame",
+        "is making Okusora Ayane even more happier than ever, after all fusses given by her friends",
 
-        "is the hard way that Alice will take",
+        "is when Shiroko asks Sensei to rob a bank, which Sensei said no",
+        
+        "is when Sensei calls Rin Nanagami, Rin-chan (or Rinny in English)",
+
+        "is when Sensei misses his wife (President of Blue Archive, Student Council President), and said: I miss my wife",
+
+        "is making Suzumi Morizuki and Kayoko Onikata more happier than ever",
+
+        "is making The Academy City of Kivotos more happier than ever",
+
+        "is making Yuuka Hayase more happier than ever",
+
+        "is Marisa Kirisame",
 
         "is love that intertwines with the interest of geographics, broadcasting and the rules of the nation",
 
         "is acceptable in the Internet age",
 
         "is Alice Margatroid's way to not getting hit with the shovel",
+
+        "is Aris Tendou's way to complete her sidequests",
 
         "is not theft",
 
@@ -30,9 +50,16 @@ const texts =
 
         "multiplies by more",
 
+        "is when people understands that it's actually free to copy",
+
         "approved",
     ];
     
     const randomIndex = Math.floor(Math.random() * texts.length);
 
-    document.getElementById('copyheart-statement').innerText = texts[randomIndex];
+    document.getElementById('copyheart-statement').innerHTML = texts[randomIndex];
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+anchor.addEventListener('click', function(e) 
+{e.preventDefault();document.querySelector(this.getAttribute('href')).scrollIntoView(
+  {behavior: 'smooth'});});});
